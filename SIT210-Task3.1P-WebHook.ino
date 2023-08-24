386f81399908317d73a125d5cf9f80ddd7eba021
prb616 <127139401+prb616@users.noreply.github.com>
2023-08-25 02:53:56 +0530

    Update SIT210-Task3.1P-WebHook.ino

--- SIT210-Task3.1P-WebHook.ino
+++ SIT210-Task3.1P-WebHook.ino
@@ -3,49 +3,57 @@
 #include <ThingSpeak.h>
 #include <DHT.h>
 
-#define DHTPIN 2      // DHT11 sensor connected to digital pin 2
-#define DHTTYPE DHT11 // DHT type
+// Pin configuration for DHT11 sensor
+#define DHTPIN 2
+#define DHTTYPE DHT11
 
+// WiFi credentials and ThingSpeak API key
 #define SECRET_SSID "@prabh"
 #define SECRET_PASS "0987654321"
 #define SECRET_API_KEY "47JFWCVHFQZCQ8QU"
 
+// Initialize the DHT sensor
 DHT dht(DHTPIN, DHTTYPE);
 
+// WiFi credentials for connecting to the network
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
-unsigned long channelID = 2250596; // Replace with your ThingSpeak Channel ID
+
+// ThingSpeak Channel configuration
+unsigned long channelID = 2250596;  //ThingSpeak Channel ID
 const char *apiKey = SECRET_API_KEY;
 
 WiFiClient wifiClient;
 
 void setup() {
-  Serial.begin(115200);
-  dht.begin();
-  connectToWiFi();
+  Serial.begin(115200); // Start serial communication for debugging
+  dht.begin(); // Initialize the DHT sensor
+  connectToWiFi(); // Connect to WiFi network
 }
 
 void loop() {
+  // Read temperature and humidity from DHT sensor
   float temperature = dht.readTemperature();
   float humidity = dht.readHumidity();
 
+  // Check if sensor readings are valid
   if (isnan(temperature) || isnan(humidity)) {
     Serial.println("Failed to read from DHT sensor!");
     delay(2000);
     return;
   }
 
-  Serial.print("Temperature: ");
-  Serial.print(temperature);
-  Serial.print("°C, Humidity: ");
-  Serial.print(humidity);
-  Serial.println("%");
-
+  // Initialize ThingSpeak communication
   ThingSpeak.begin(wifiClient);
+
+  // Set temperature and humidity values as fields
   ThingSpeak.setField(1, temperature);
   ThingSpeak.setField(2, humidity);
+
+  // Send data to ThingSpeak and get response
   int response = ThingSpeak.writeFields(channelID, apiKey);
 
+  // Check if data was successfully sent
   if (response == 200) {
     Serial.println("Data sent to ThingSpeak");
   } else {
@@ -55,13 +63,12 @@ void loop() {
   delay(60000); // Wait for 1 minute before reading again
 }
 
+// Function to connect to WiFi network
 void connectToWiFi() {
   int status = WL_IDLE_STATUS;
   while (status != WL_CONNECTED) {
-    Serial.print("Connecting to WiFi...");
+    // Attempt to connect to WiFi using provided credentials
     status = WiFi.begin(ssid, pass);
     delay(500);
   }
-  Serial.println();
-  Serial.println("Connected to WiFi");
 }
